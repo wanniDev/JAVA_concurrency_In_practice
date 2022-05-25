016237ac8c11cc33b8284098a62be425b6b657b0
wanniDev <wannidev0928@gmail.com>
2022-05-26 07:03:11 +0900

    Feat: ReaderThread

---
+++ src/main/java/me/java/ch2/sub02cancellation_shutdown/ReaderThread.java
@@ -0,0 +1,44 @@
+package me.java.ch2.sub02cancellation_shutdown;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Socket;
+
+public class ReaderThread extends Thread {
+	private final Socket socket;
+	private final InputStream in;
+
+	public ReaderThread(Socket socket) throws IOException {
+		this.socket = socket;
+		this.in = socket.getInputStream();
+	}
+
+	@Override
+	public void interrupt() {
+		try {
+			socket.close();
+		} catch (IOException ignored) {
+
+		} finally {
+			super.interrupt();
+		}
+	}
+
+	@Override
+	public void run() {
+		try {
+			byte[] buf = new byte[1024];
+			while (true) {
+				int count = in.read(buf);
+				if (count < 0)
+					break;
+				else if (count > 0)
+					processBuffer(buf, count);
+			}
+		} catch (IOException e) {
+
+		}
+	}
+
+	private void processBuffer(byte[] buf, int count) {}
+}
