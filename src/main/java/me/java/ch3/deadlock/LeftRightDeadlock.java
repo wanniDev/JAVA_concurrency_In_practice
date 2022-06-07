ae0cd71a3a905b4ff4ac78e8604e01406a191655
wanniDev <wannidev0928@gmail.com>
2022-06-07 23:41:59 +0900

    순서에 의한 락 대기는 데드락 문제를 발생시킬 수 있다.

---
+++ src/main/java/me/java/ch3/deadlock/LeftRightDeadlock.java
@@ -0,0 +1,30 @@
+package me.java.ch3.deadlock;
+
+public class LeftRightDeadlock {
+	private final Object left = new Object();
+	private final Object right = new Object();
+
+	public void leftRight() {
+		synchronized (left) {
+			synchronized (right) {
+				doSomething();
+			}
+		}
+	}
+
+	private void doSomething() {
+
+	}
+
+	private void rightLeft() {
+		synchronized (right) {
+			synchronized (left) {
+				doSomethingElse();
+			}
+		}
+	}
+
+	private void doSomethingElse() {
+		
+	}
+}
